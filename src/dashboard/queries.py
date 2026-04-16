3b5dfdc225069c4c7f4aaffae865010742154e7e
rayluo88 <raymond.luo0902@gmail.com>
2026-04-16 17:41:12 +0800

    Feat: add station name tooltips to crowd density dots on operations page

--- src/dashboard/queries.py
+++ src/dashboard/queries.py
@@ -11,6 +11,95 @@ from src.db.models import AnomalyEvent, LtaCrowdDensity, LtaDisruption, LtaFacil
 
 TOTAL_FLEET = 200  # Approximate MRT fleet across NSL, EWL, CCL, DTL, NEL
 
+# Singapore MRT/LRT station code → name mapping
+STATION_NAMES: dict[str, str] = {
+    # North South Line (NSL)
+    "NS1": "Jurong East", "NS2": "Bukit Batok", "NS3": "Bukit Gombak",
+    "NS4": "Choa Chu Kang", "NS5": "Yew Tee", "NS7": "Kranji",
+    "NS8": "Marsiling", "NS9": "Woodlands", "NS10": "Admiralty",
+    "NS11": "Sembawang", "NS12": "Canberra", "NS13": "Yishun",
+    "NS14": "Khatib", "NS15": "Yio Chu Kang", "NS16": "Ang Mo Kio",
+    "NS17": "Bishan", "NS18": "Braddell", "NS19": "Toa Payoh",
+    "NS20": "Novena", "NS21": "Newton", "NS22": "Orchard",
+    "NS23": "Somerset", "NS24": "Dhoby Ghaut", "NS25": "City Hall",
+    "NS26": "Raffles Place", "NS27": "Marina Bay", "NS28": "Marina South Pier",
+    # East West Line (EWL)
+    "EW1": "Pasir Ris", "EW2": "Tampines", "EW3": "Simei",
+    "EW4": "Tanah Merah", "EW5": "Bedok", "EW6": "Kembangan",
+    "EW7": "Eunos", "EW8": "Paya Lebar", "EW9": "Aljunied",
+    "EW10": "Kallang", "EW11": "Lavender", "EW12": "Bugis",
+    "EW13": "City Hall", "EW14": "Raffles Place", "EW15": "Tanjong Pagar",
+    "EW16": "Outram Park", "EW17": "Tiong Bahru", "EW18": "Redhill",
+    "EW19": "Queenstown", "EW20": "Commonwealth", "EW21": "Buona Vista",
+    "EW22": "Dover", "EW23": "Clementi", "EW24": "Jurong East",
+    "EW25": "Chinese Garden", "EW26": "Lakeside", "EW27": "Boon Lay",
+    "EW28": "Pioneer", "EW29": "Joo Koon", "EW30": "Gul Circle",
+    "EW31": "Tuas Crescent", "EW32": "Tuas West Road", "EW33": "Tuas Link",
+    # Changi branch
+    "CG1": "Expo", "CG2": "Changi Airport",
+    # North East Line (NEL)
+    "NE1": "HarbourFront", "NE3": "Outram Park", "NE4": "Chinatown",
+    "NE5": "Clarke Quay", "NE6": "Dhoby Ghaut", "NE7": "Little India",
+    "NE8": "Farrer Park", "NE9": "Boon Keng", "NE10": "Potong Pasir",
+    "NE11": "Woodleigh", "NE12": "Serangoon", "NE13": "Kovan",
+    "NE14": "Hougang", "NE15": "Buangkok", "NE16": "Sengkang",
+    "NE17": "Punggol",
+    # Circle Line (CCL)
+    "CC1": "Dhoby Ghaut", "CC2": "Bras Basah", "CC3": "Esplanade",
+    "CC4": "Promenade", "CC5": "Nicoll Highway", "CC6": "Stadium",
+    "CC7": "Mountbatten", "CC8": "Dakota", "CC9": "Paya Lebar",
+    "CC10": "MacPherson", "CC11": "Tai Seng", "CC12": "Bartley",
+    "CC13": "Serangoon", "CC14": "Lorong Chuan", "CC15": "Bishan",
+    "CC16": "Marymount", "CC17": "Caldecott", "CC19": "Botanic Gardens",
+    "CC20": "Farrer Road", "CC21": "Holland Village", "CC22": "Buona Vista",
+    "CC23": "one-north", "CC24": "Kent Ridge", "CC25": "Haw Par Villa",
+    "CC26": "Pasir Panjang", "CC27": "Labrador Park", "CC28": "Telok Blangah",
+    "CC29": "HarbourFront",
+    # Circle Line Extension (CEL)
+    "CE1": "Bayfront", "CE2": "Marina Bay",
+    # Downtown Line (DTL)
+    "DT1": "Bukit Panjang", "DT2": "Cashew", "DT3": "Hillview",
+    "DT5": "Beauty World", "DT6": "King Albert Park",
+    "DT7": "Sixth Avenue", "DT8": "Tan Kah Kee", "DT9": "Botanic Gardens",
+    "DT10": "Stevens", "DT11": "Newton", "DT12": "Little India",
+    "DT13": "Rochor", "DT14": "Bugis", "DT15": "Promenade",
+    "DT16": "Bayfront", "DT17": "Downtown", "DT18": "Telok Ayer",
+    "DT19": "Chinatown", "DT20": "Fort Canning", "DT21": "Bencoolen",
+    "DT22": "Jalan Besar", "DT23": "Bendemeer", "DT24": "Geylang Bahru",
+    "DT25": "Mattar", "DT26": "MacPherson", "DT27": "Ubi",
+    "DT28": "Kaki Bukit", "DT29": "Bedok North", "DT30": "Bedok Reservoir",
+    "DT31": "Tampines West", "DT32": "Tampines", "DT33": "Tampines East",
+    "DT34": "Upper Changi", "DT35": "Expo",
+    # Thomson-East Coast Line (TEL)
+    "TE1": "Woodlands North", "TE2": "Woodlands", "TE3": "Woodlands South",
+    "TE4": "Springleaf", "TE5": "Lentor", "TE6": "Mayflower",
+    "TE7": "Bright Hill", "TE8": "Upper Thomson", "TE9": "Caldecott",
+    "TE11": "Stevens", "TE12": "Napier", "TE13": "Orchard Boulevard",
+    "TE14": "Orchard", "TE15": "Great World", "TE16": "Havelock",
+    "TE17": "Outram Park", "TE18": "Maxwell", "TE19": "Shenton Way",
+    "TE20": "Marina Bay", "TE22": "Gardens by the Bay",
+    "TE23": "Tanjong Rhu", "TE24": "Katong Park", "TE25": "Tanjong Katong",
+    "TE26": "Marine Parade", "TE27": "Marine Terrace", "TE28": "Siglap",
+    "TE29": "Bayshore",
+    # Bukit Panjang LRT (BPL)
+    "BP1": "Choa Chu Kang", "BP2": "South View", "BP3": "Keat Hong",
+    "BP4": "Teck Whye", "BP5": "Phoenix", "BP6": "Bukit Panjang",
+    "BP7": "Petir", "BP8": "Pending", "BP9": "Bangkit",
+    "BP10": "Fajar", "BP11": "Segar", "BP12": "Jelapang",
+    "BP13": "Senja", "BP14": "Ten Mile Junction",
+    # Sengkang LRT (SLRT)
+    "SE1": "Sengkang", "SE2": "Compassvale", "SE3": "Rumbia",
+    "SE4": "Bakau", "SE5": "Kangkar",
+    "SW1": "Sengkang", "SW2": "Thanggam", "SW3": "Fernvale",
+    "SW4": "Layar", "SW5": "Tongkang", "SW6": "Renjong",
+    "SW7": "Cheng Lim", "SW8": "Farmway",
+    # Punggol LRT (PLRT)
+    "PE1": "Punggol", "PE2": "Meridian", "PE3": "Coral Edge",
+    "PE4": "Riviera", "PE5": "Kadaloor", "PE6": "Oasis", "PE7": "Damai",
+    "PW1": "Punggol", "PW3": "Sam Kee", "PW4": "Punggol Point",
+    "PW5": "Samudera", "PW6": "Nibong", "PW7": "Sumang",
+}
+
 LINE_INFO = {
     "NSL": "North South Line",
     "EWL": "East West Line",
@@ -191,6 +280,7 @@ def get_operations_data(db: Session) -> dict:
         "crowd_by_line": crowd_by_line,
         "facilities": facilities,
         "has_lta_data": bool(disruptions or latest_crowd or facilities),
+        "station_names": STATION_NAMES,
     }
 
 
